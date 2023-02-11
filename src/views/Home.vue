b31f9e2c932619951c1a1065b4d01e0b44fb84d3
emsitkowski <emsitkowski@gmail.com>
2023-02-11 15:10:40 +0100

    hid the headers + adjusted copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-full">
-    <h1>Join the chatroom</h1>
+    <!--     <h1>Join the chatroom</h1> -->
     <AuthSection type="login">
       <template v-slot:header>Log in</template>
       <template v-slot:subheader>Join the chatroom and meet awesome people just now!</template>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="container-full">
-    <h1>Get started</h1>
+    <!--     <h1>Create an account</h1> -->
     <AuthSection type="register">
       <template v-slot:header>Register</template>
-      <template v-slot:buttonText>create an account</template>
+      <template v-slot:subheader>Create new account and join our awesome community!</template>
+      <template v-slot:button>
+        <Button type="submit" text="Create an account" class="primary" />
+        <router-link to="/">
+          <Button text="Log in" class="secondary" />
+        </router-link>
+      </template>
     </AuthSection>
   </div>
 </template>
 
 <script setup>
 import AuthSection from "../components/AuthSection.vue";
+import Button from "../components/Button.vue";
 </script>
 
 <style lang="scss" scoped></style>
