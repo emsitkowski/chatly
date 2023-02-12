d497b45737c259680ec6c6417aae1e12e1c21172
emsitkowski <emsitkowski@gmail.com>
2023-02-12 13:18:14 +0100

    lazy loading image component added

---
+++ src/components/LazyImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <img class="fade-in" :src="src" :alt="alt" loading="lazy" />
+</template>
+
+<script setup>
+import { initTransitions } from "../composables/transitions";
+import { defineProps } from "vue";
+
+initTransitions();
+defineProps({
+  src: String,
+  alt: String,
+});
+</script>
+
+<style lang="scss" scoped></style>
