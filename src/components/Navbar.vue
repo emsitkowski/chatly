e6f315856d14b7905f77023afc4ee6417fa305ca
emsitkowski <emsitkowski@gmail.com>
2023-03-09 20:41:21 +0100

    navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="@/assets/img/chatly-logo.svg" alt="" />
+    </router-link>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+nav {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  padding: 24px 12px;
+  margin: 0 16px;
+  background: rgba(255, 255, 255, 0.06);
+  top: 16px;
+}
+</style>
